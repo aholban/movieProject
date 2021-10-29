1f15e717dd3aef031b26e9f426a5063c17d66d90
aholban <andreeaholban98@gmail.com>
2021-10-29 16:38:09 +0100

    Movie has now a rating and length, getters and setters are tested

--- src/main/java/com/softwareinstitute/andreeaholban/moviesProject/Movie.java
+++ src/main/java/com/softwareinstitute/andreeaholban/moviesProject/Movie.java
@@ -4,11 +4,18 @@ public class Movie {
 
     ///////////////////////////////////////////Attributes//////////////////////////////////////////////////////////////////////
     private String title;
+    private double rating;
+    private int length;
 
 
     ///////////////////////////////////////////Constructors////////////////////////////////////////////////////////////////////
     public Movie(String title){
         this.title = title;
+    }
+
+    public Movie(String title, int length){
+        this.title = title;
+        this.length = length;
     }
 
     //////////////////////////////////////////////Methods/////////////////////////////////////////////////////////////////////
@@ -20,6 +27,22 @@ public class Movie {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
     }
 
     public String getJasonObjectOfMovie(){
--- src/test/java/com/softwareinstitute/andreeaholban/moviesProject/MovieTest.java
+++ src/test/java/com/softwareinstitute/andreeaholban/moviesProject/MovieTest.java
@@ -21,6 +21,34 @@ public class MovieTest {
     }
 
     @Test
+    public void testGetRating(){
+        Movie movie = new Movie("Lord of the rings");
+        double movieRating = movie.getRating();
+        assertEquals(0, movieRating, "Wrong rating");
+    }
+
+    @Test
+    public void testSetRating(){
+        Movie movie = new Movie("Lord of the rings");
+        movie.setRating(9.7);
+        assertEquals(9.7, movie.getRating(), "Set wrong rating");
+    }
+
+    @Test
+    public void testGetLength(){
+        Movie movie = new Movie("Lord of the rings", 120);
+        int movieLength = movie.getLength();
+        assertEquals(120, movieLength, "Wrong length");
+    }
+
+    @Test
+    public void testSetLength(){
+        Movie movie = new Movie("Lord of the rings");
+        movie.setLength(120);
+        assertEquals(120, movie.getLength(), "Set wrong length");
+    }
+
+    @Test
     public void testGetJasonObject(){
         Movie movie = new Movie("Lord of the rings");
         String json = movie.getJasonObjectOfMovie();
