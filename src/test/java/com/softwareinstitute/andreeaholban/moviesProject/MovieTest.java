dd9eb745fd6691dfdc2af3b94a461b8f28f205ce
aholban <andreeaholban98@gmail.com>
2021-10-29 16:46:00 +0100

    Resolving some code smells

--- src/test/java/com/softwareinstitute/andreeaholban/moviesProject/MovieTest.java
+++ src/test/java/com/softwareinstitute/andreeaholban/moviesProject/MovieTest.java
@@ -4,52 +4,52 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MovieTest {
+class MovieTest {
 
     @Test
-    public void testGetTitle(){
+    void testGetTitle(){
         Movie movie = new Movie("Lord of the rings");
         String movieTitle = movie.getTitle();
         assertEquals("Lord of the rings", movieTitle, "Wrong title");
     }
 
     @Test
-    public void testSetTitle(){
+    void testSetTitle(){
         Movie movie = new Movie("Lord of the rings");
         movie.setTitle("Harry Potter");
         assertEquals("Harry Potter", movie.getTitle(), "Set wrong title");
     }
 
     @Test
-    public void testGetRating(){
+    void testGetRating(){
         Movie movie = new Movie("Lord of the rings");
         double movieRating = movie.getRating();
         assertEquals(0, movieRating, "Wrong rating");
     }
 
     @Test
-    public void testSetRating(){
+    void testSetRating(){
         Movie movie = new Movie("Lord of the rings");
         movie.setRating(9.7);
         assertEquals(9.7, movie.getRating(), "Set wrong rating");
     }
 
     @Test
-    public void testGetLength(){
+    void testGetLength(){
         Movie movie = new Movie("Lord of the rings", 120);
         int movieLength = movie.getLength();
         assertEquals(120, movieLength, "Wrong length");
     }
 
     @Test
-    public void testSetLength(){
+    void testSetLength(){
         Movie movie = new Movie("Lord of the rings");
         movie.setLength(120);
         assertEquals(120, movie.getLength(), "Set wrong length");
     }
 
     @Test
-    public void testGetJasonObject(){
+    void testGetJasonObject(){
         Movie movie = new Movie("Lord of the rings");
         String json = movie.getJasonObjectOfMovie();
         assertEquals("{\"title\" : \"Lord of the rings\"}", json, "Generated wrong jason file");
